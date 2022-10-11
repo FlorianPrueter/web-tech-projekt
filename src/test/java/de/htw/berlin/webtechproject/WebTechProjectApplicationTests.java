52fa975034296d4c49915e909547e5dd9306104d
FlorianPrueter <florianprueter@gmx.de>
2022-10-11 13:12:47 +0200

    initial project setup

---
+++ src/test/java/de/htw/berlin/webtechproject/WebTechProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htw.berlin.webtechproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTechProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
