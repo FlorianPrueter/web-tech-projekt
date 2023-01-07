ee0904cafac816d606981b9d32aafc8a17cb38b7
tobiasew <101264085+tobiasew@users.noreply.github.com>
2023-01-07 20:32:58 +0100

    https -> http

--- src/main/java/de/htw/berlin/webtechproject/config/WebConfig.java
+++ src/main/java/de/htw/berlin/webtechproject/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/api/**")
                 .allowedMethods("*")
                 .allowedOrigins(
-                                "https://http://localhost:3000",
+                                "http://localhost:3000",
                                 "https://web-tech-projekt-frontend.herokuapp.com"
                 );
     }
